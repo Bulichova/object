289fc6ca45abbeca6790f1d7386714de6efa4056
ManellDmitry <manell@ukr.net>
2021-12-25 01:33:18 +0200

    final to get widget v2

--- js/getWeatherData.js
+++ js/getWeatherData.js
@@ -16,13 +16,11 @@ export function getWeatherData(city, apiId, innerPlace) {
       if (data) {
         let markup = "";
         if (data.weather.length > 0) {
-          let weatherIconMarkup = data.weather.map(({ icon, description }) => {
-            if (icon && description) {
-              return `<img width="20" src="http://openweathermap.org/img/wn/${icon}@2x.png" alt="${description}/"><span>${description}</span>`;
-            } else {
-              return null;
-            }
-          });
+          let weatherIconMarkup = data.weather.map(({ icon, description }) =>
+            icon && description
+              ? `<img width="20" src="http://openweathermap.org/img/wn/${icon}@2x.png" alt="${description}/"><span>${description}</span>`
+              : null
+          );
           if (weatherIconMarkup) {
             weatherIconMarkup = weatherIconMarkup.join("");
             markup = `<div><h2>${
