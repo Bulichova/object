3d4cac89849e227cec6eb73d3a6162a5eb0fb6e2
ManellDmitry <manell@ukr.net>
2021-12-25 01:27:20 +0200

    final to get widget

--- js/getWeatherData.js
+++ js/getWeatherData.js
@@ -9,34 +9,39 @@ export function getWeatherData(city, apiId, innerPlace) {
       return result.json();
     })
     .then((data) => {
-      if (data.cod != 202) {
+      if (data.cod != 200) {
         alert(`${data.message}`);
         return;
       }
       if (data) {
         let markup = "";
-        let weatherIconMarkup;
         if (data.weather.length > 0) {
-          weatherIconMarkup = data.weather.map(({ icon, description }) => {
+          let weatherIconMarkup = data.weather.map(({ icon, description }) => {
             if (icon && description) {
               return `<img width="20" src="http://openweathermap.org/img/wn/${icon}@2x.png" alt="${description}/"><span>${description}</span>`;
             } else {
               return null;
             }
           });
-
-          markup = `<div><h2>${data.name}</h2>${weatherIconMarkup}${Math.round(
-            data.main.temp - 273.15
-          )}&deg;C</p></div>`;
+          if (weatherIconMarkup) {
+            weatherIconMarkup = weatherIconMarkup.join("");
+            markup = `<div><h2>${
+              data.name
+            }</h2>${weatherIconMarkup}${Math.round(
+              data.main.temp - 273.15
+            )}&deg;C</p></div>`;
+          } else {
+            markup = `<div><h2>${data.name}</h2><p>${Math.round(
+              data.main.temp - 273.15
+            )}&deg;C</p></div>`;
+          }
         } else {
           markup = `<div><h2>${data.name}</h2><p>${Math.round(
             data.main.temp - 273.15
           )}&deg;C</p></div>`;
         }
-        document
-          .querySelector(".widget")
-          .insertAdjacentHTML("afterbegin", markup);
-        return true;
+        innerPlace.insertAdjacentHTML("afterbegin", markup);
+        return data;
       }
     })
     .catch((error) => error);
